27bbd92221c160f0d35b844a9ef1225873dd9c57
Omer Karlidag <omerkarlidag23@gmail.com>
2024-04-30 17:47:42 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Java ile faktöriyel hesaplayan program yazıyoruz.
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Faktöriyel Sayısı : ");
+        int n = scan.nextInt();
+        int total = 1;
+
+        for (int i = 1; i <= n; i++) {
+            total = total * i;
+        }
+        System.out.println(n + ". Faktöriyel : " + total);
+    }
+}
